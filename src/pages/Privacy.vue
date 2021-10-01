ca2ad91fe755235464ed0b712877721e7a5981bd
eru123 <yeoligoakino@gmail.com>
2021-10-01 23:30:00 +0800

    Added Privacy Policy page

--- src/pages/Privacy.vue
+++ src/pages/Privacy.vue
@@ -1,3 +1,22 @@
 <template>
-  <h1>Privacy Policy</h1>
+  <Back @back="$router.go(-1)" />
+  <div class="quad-article">
+    <h1>Privacy Policy</h1>
+    <p>
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Soluta recusandae eaque quibusdam, velit inventore eos. Cum vitae quia, itaque omnis nemo sed veniam veritatis eveniet a, quod iure repellendus nihil.
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Soluta recusandae eaque quibusdam, velit inventore eos. Cum vitae quia, itaque omnis nemo sed veniam veritatis eveniet a, quod iure repellendus nihil.
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Soluta recusandae eaque quibusdam, velit inventore eos. Cum vitae quia, itaque omnis nemo sed veniam veritatis eveniet a, quod iure repellendus nihil.
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Soluta recusandae eaque quibusdam, velit inventore eos. Cum vitae quia, itaque omnis nemo sed veniam veritatis eveniet a, quod iure repellendus nihil.
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Soluta recusandae eaque quibusdam, velit inventore eos. Cum vitae quia, itaque omnis nemo sed veniam veritatis eveniet a, quod iure repellendus nihil.
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Soluta recusandae eaque quibusdam, velit inventore eos. Cum vitae quia, itaque omnis nemo sed veniam veritatis eveniet a, quod iure repellendus nihil.
+    </p>
+  </div>
 </template>
+<script>
+import Back from '../components/Back.vue'
+export default {
+  components: {
+    Back
+  }
+}
+</script>
