0a8aa741a6527f31483b3600f17ae3e20274af05
eru123 <yeoligoakino@gmail.com>
2021-10-02 01:58:25 +0000

    yarn

--- src/main.js
+++ src/main.js
@@ -2,12 +2,10 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './utils/router'
 import components from './utils/globalComponents'
-import VueEllipseProgress from 'vue-ellipse-progress'
 import './styles/index.scss'
 import '@fortawesome/fontawesome-free/css/all.css'
 const app = createApp(App)
 
 app.use(router)
 app.use(components)
-app.use(VueEllipseProgress)
 app.mount('#app')
